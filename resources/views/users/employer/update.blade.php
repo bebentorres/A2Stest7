13db4a23d972e562f0012867acc65df5e6767d1b
MelvinGesulgon <freelancemgesulgon@gmail.com>
2022-11-05 08:34:50 +0800

    conflict resolve

---
+++ resources/views/users/employer/update.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <form action="{{route('employer.update')}}" method="POST" enctype="multipart/form-data">
+        {!! csrf_field() !!}
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Update Company Information</h2>
+                <input type="text" hidden value="{{Auth::user()->employerprofile->id}}" name="user_id">
+                <br>
+                <label for="cname" >Company Name</label>
+                <input type="text" class="form-control" name="cname" value="{{Auth::user()->employerprofile->cname}}">
+                <br>
+                <label for="location" >Location:</label>
+                <input type="text" class="form-control" name="location" value="{{Auth::user()->employerprofile->location}}">
+                <br>
+                <label for="cnum" >Number:</label>
+                <input type="text" class="form-control" name="cnum" value="{{Auth::user()->employerprofile->cnum}}">
+                <br>
+                <label for="description">Company Description</label>
+                <textarea class="form-control" name="description" value="{{Auth::user()->employerprofile->description}}"></textarea>
+                <br>
+                <label for="">Company Logo</label><br>
+                <input type="file" class="form-control" name="" id="">
+                <br>
+                <hr>
+                <h3>Additional Information</h3>
+                <label for="benefits" >Benefits:</label>
+                <input type="text" class="form-control" name="benefits" value="{{Auth::user()->employerprofile->benefits}}">
+                <br>
+                <label for="workhours" >Work Hours:</label>
+                <input type="text" class="form-control" name="workhours" value="{{Auth::user()->employerprofile->workhours}}">
+                <br>
+                <label for="worksetup" >Work Setup:</label>
+                <input type="text" class="form-control" name="worksetup" value="{{Auth::user()->employerprofile->worksetup}}">
+                <br>
+                <label for="comsize" >Company size:</label>
+                <input type="text" class="form-control" name="comsize" value="{{Auth::user()->employerprofile->comsize}}">
+                <br>
+                <label for="dcode" >Dress code:</label>
+                <input type="text" class="form-control" name="dcode" value="{{Auth::user()->employerprofile->dcode}}">
+                <br>
+            </div>
+        </div>
+        
+        <button type="submit" class="btn btn-success">
+            {{ __('Submit') }}
+        </button>
+    
+        <div>
+            @if(Session::has('message'))
+                <div class="alert alert-success">
+                    {{Session::get('message')}}
+                </div>
+            @endif
+        </div>
+    
+    </form>
+    
+</div>
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -29,22 +29,28 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/jobseeker' ,'App\Http\Controllers\JobseekerProfileController@index');
 Route::get('/jobseeker/editprofile' ,'App\Http\Controllers\JobseekerProfileController@edit');
 Route::post('/jobseeker/editprofile/store' ,'App\Http\Controllers\JobseekerProfileController@editprofile')->name('jobseeker.store');
+
+Route::get('/jobseeker/editprofile/update', 'App\Http\Controllers\JobseekerProfileController@up' );
 Route::post('/jobseeker/editprofile/update' ,'App\Http\Controllers\JobseekerProfileController@update')->name('jobseeker.update');
+
 
 
 // Employer
 Route::get('/employer' ,'App\Http\Controllers\EmployerProfileController@index');
 Route::get('/employer/editprofile' ,'App\Http\Controllers\EmployerProfileController@edit');
 Route::post('/employer/editprofile/store' ,'App\Http\Controllers\EmployerProfileController@editprofile')->name('employer.store');
+
+Route::get('/employer/editprofile/update', 'App\Http\Controllers\EmployerProfileController@up' );
+Route::post('/employer/editprofile/update' ,'App\Http\Controllers\EmployerProfileController@update')->name('employer.update');
+
 //Route::post('/employer/editprofile/store' ,'App\Http\Controllers\EmployerProfileController@store');
 
-//added route
-Route::get('edit/{job_id}',['EmployerProfileController::class, edit']);
+// Post
+Route::view('/employer/createpost', 'users.employer.createjob')->name('employer.create');
 
 
-//Job Posts
-// Route::resource('job_posts', 'App\Http\Controllers\JobPostController');
-// Route::get('create', 'App\Http\Controllers\JobPostController@create');
-// Route::get('jobpost', 'App\Http\Controllers\JobPostController@index');
+// Search page
+Route::view('/search', 'search');
+
 
 Route::resource('jobPosts', 'App\Http\Controllers\JobPostController');
